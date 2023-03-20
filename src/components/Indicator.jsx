4d2496728bf429c4cd7425e1dad7f11ea319c1ef
Laurent NOËL <lnoel4289@gmail.com>
2023-03-21 00:02:29 +0100

    indicator css

--- src/components/Indicator.jsx
+++ src/components/Indicator.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Indicator = ({ pictures, picture }) => {
   return (
-    <div>
+    <div className="indicator">
       {picture + 1}/{pictures.length}
     </div>
   );

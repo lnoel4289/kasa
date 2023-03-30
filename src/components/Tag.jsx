e9e156b63bb2584439c65aa57ea243596efe59b8
Laurent NOËL <lnoel4289@gmail.com>
2023-03-30 20:33:20 +0200

    practice class components

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import /*React,*/ { Component } from "react";
 
-const Tag = ({ tag }) => {
-  return (
-    <div className="tag">
-      <h3>{tag}</h3>
-    </div>
-  );
-};
+// const Tag = ({ tag }) => {
+//   return (
+//     <div className="tag">
+//       <h3>{tag}</h3>
+//     </div>
+//   );
+// };
+
+// test class component
+class Tag extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { tag } = this.props;
+    return (
+      <div className="tag">
+        <h3>{tag}</h3>
+      </div>
+    );
+  }
+}
 
 export default Tag;

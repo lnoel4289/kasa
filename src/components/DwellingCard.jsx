6519fe938a0e8b145cfd27cbaf8010f70bf33028
Laurent NOËL <lnoel4289@gmail.com>
2023-04-20 18:13:55 +0200

    improving jsx

--- src/components/DwellingCard.jsx
+++ src/components/DwellingCard.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const DwellingCard = ({ dwelling }) => {
+  const { title, id, cover } = dwelling;
   return (
-    <Link className="dwellingCard" to={`/dwelling/${dwelling.id}`}>
-      <h2>{dwelling.title}</h2>
-      <img src={`${dwelling.cover}`} alt={`${dwelling.title}`} />
+    <Link className="dwellingCard" to={`/dwelling/${id}`}>
+      <h2>{title}</h2>
+      <img src={`${cover}`} alt={`${title}`} />
     </Link>
   );
 };
